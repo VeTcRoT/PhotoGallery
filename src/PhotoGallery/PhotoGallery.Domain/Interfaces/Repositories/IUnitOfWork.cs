cfb416206d9acf99e38747f52162833a9a79a441
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-07 15:54:36 +0300

    Add new property to UoW regarding new repo

--- src/PhotoGallery/PhotoGallery.Domain/Interfaces/Repositories/IUnitOfWork.cs
+++ src/PhotoGallery/PhotoGallery.Domain/Interfaces/Repositories/IUnitOfWork.cs
@@ -5,6 +5,7 @@ namespace PhotoGallery.Domain.Interfaces.Repositories
     public interface IUnitOfWork
     {
         IAlbumRepository AlbumRepository { get; }
+        IImageRepository ImageRepository { get; }
         IBaseRepository<TEntity> GetRepository<TEntity>()
             where TEntity : EntityBase;
         Task<int> SaveChangesAsync();
