c19a17272c79053a2473dc1380364cb1f512e7d3
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-07 10:40:51 +0300

    Add properties to Album entity

--- PhotoGallery/PhotoGallery.Domain/Entities/Album.cs
+++ PhotoGallery/PhotoGallery.Domain/Entities/Album.cs
@@ -2,5 +2,14 @@
 {
     public class Album
     {
+        public int Id { get; set; }
+        public int Title { get; set; }
+        public int Description { get; set; }
+        public string FolderName { get; set; } = string.Empty;
+
+        public ICollection<Image> Images { get; set; } = 
+            new List<Image>();
+        public ICollection<UserAlbum> UserAlbums { get; set; } = 
+            new List<UserAlbum>();
     }
 }
