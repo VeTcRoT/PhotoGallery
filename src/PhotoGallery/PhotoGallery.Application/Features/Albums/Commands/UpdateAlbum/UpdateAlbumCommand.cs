26398ad800c40569dd2dd02f43fb5c8fd6716859
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-07 12:32:41 +0300

    Implement updation of album

---
+++ src/PhotoGallery/PhotoGallery.Application/Features/Albums/Commands/UpdateAlbum/UpdateAlbumCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace PhotoGallery.Application.Features.Albums.Commands.UpdateAlbum
+{
+    public class UpdateAlbumCommand : IRequest<UpdateAlbumDto>
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ src/PhotoGallery/PhotoGallery.Application/Features/Albums/Commands/UpdateAlbum/UpdateAlbumDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PhotoGallery.Application.Features.Albums.Commands.UpdateAlbum
+{
+    public class UpdateAlbumDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
