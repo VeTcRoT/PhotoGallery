15fb59c6d419091fa5eb87e6cb8f0b72ffef683f
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-07 16:47:42 +0300

    Add User service

---
+++ src/PhotoGallery/PhotoGallery.Application/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PhotoGallery.Domain.Entities;
+using PhotoGallery.Domain.Interfaces.Services;
+
+namespace PhotoGallery.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<bool> IsAdmin(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            return await _userManager.IsInRoleAsync(user, "Admin");
+        }
+
+        public async Task<bool> UserExists(string userId)
+        {
+            return await _userManager.FindByIdAsync(userId) != null;
+        }
+    }
+}
---
+++ src/PhotoGallery/PhotoGallery.Domain/Interfaces/Services/IUserService.cs
@@ -0,0 +1,8 @@
+﻿namespace PhotoGallery.Domain.Interfaces.Services
+{
+    public interface IUserService
+    {
+        Task<bool> UserExists(string userId);
+        Task<bool> IsAdmin(string userId);
+    }
+}
