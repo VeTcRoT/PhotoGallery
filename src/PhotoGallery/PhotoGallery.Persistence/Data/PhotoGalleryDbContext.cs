f96c0ea42fdc9c0b2339ffaff393f19ac142f2ab
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-07 18:17:42 +0300

    Add db context

---
+++ src/PhotoGallery/PhotoGallery.Persistence/Data/PhotoGalleryDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PhotoGallery.Domain.Entities;
+
+namespace PhotoGallery.Persistence.Data
+{
+    public class PhotoGalleryDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Album> Albums { get; set; } = null!;
+        public DbSet<Image> Images { get; set; } = null!;
+        public DbSet<Rate> Rates { get; set; } = null!;
+
+        public PhotoGalleryDbContext(DbContextOptions<PhotoGalleryDbContext> options) : base(options)
+        { }
+    }
+}
