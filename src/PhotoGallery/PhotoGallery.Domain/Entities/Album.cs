1a2741ea5dfdb894657c4d173c36340cc1f5bc9a
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-07 17:13:53 +0300

    Add UserId property to Album

--- src/PhotoGallery/PhotoGallery.Domain/Entities/Album.cs
+++ src/PhotoGallery/PhotoGallery.Domain/Entities/Album.cs
@@ -4,6 +4,7 @@
     {
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
 
         public ICollection<Image> Images { get; set; } = 
             new List<Image>();
