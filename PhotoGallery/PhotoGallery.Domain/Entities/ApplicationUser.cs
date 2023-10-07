50c592251eb0633bd6cc74d4f57178ef62410243
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-07 10:42:11 +0300

    Add proeprty to ApplicationUser entity

--- PhotoGallery/PhotoGallery.Domain/Entities/ApplicationUser.cs
+++ PhotoGallery/PhotoGallery.Domain/Entities/ApplicationUser.cs
@@ -4,5 +4,7 @@ namespace PhotoGallery.Domain.Entities
 {
     public class ApplicationUser : IdentityUser
     {
+        public ICollection<UserAlbum> UserAlbums { get; set; } = 
+            new List<UserAlbum>();
     }
 }
