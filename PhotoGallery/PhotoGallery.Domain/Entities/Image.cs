f85c8ecdc9abfd18dd45107cd5c2269b59aca71e
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-07 10:41:16 +0300

    Add proeprties to Image entity

--- PhotoGallery/PhotoGallery.Domain/Entities/Image.cs
+++ PhotoGallery/PhotoGallery.Domain/Entities/Image.cs
@@ -2,5 +2,12 @@
 {
     public class Image
     {
+        public int Id { get; set; }
+        public string FileName { get; set; } = string.Empty;
+        public int AlbumId { get; set; }
+        public uint Likes { get; set; }
+        public uint Dislikes { get; set; }
+
+        public Album Album { get; set; } = null!;
     }
 }
