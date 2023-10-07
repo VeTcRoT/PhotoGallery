ec6bd56b45afbcabe75ae610ee8226c068832c21
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-07 10:41:45 +0300

    Add proeprties to UserAlbum entity

--- PhotoGallery/PhotoGallery.Domain/Entities/UserAlbum.cs
+++ PhotoGallery/PhotoGallery.Domain/Entities/UserAlbum.cs
@@ -2,5 +2,10 @@
 {
     public class UserAlbum
     {
+        public int UserId { get; set; }
+        public int AlbumId { get; set; }
+
+        public ApplicationUser User { get; set; } = null!;
+        public Album Album { get; set; } = null!;
     }
 }
