b3463aeebc50aedb2f5364ecd6a576ab0411a107
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-07 15:55:03 +0300

    Add DeleteImage command

---
+++ src/PhotoGallery/PhotoGallery.Application/Features/Images/Commands/DeleteImage/DeleteImageCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace PhotoGallery.Application.Features.Images.Commands.DeleteImage
+{
+    public class DeleteImageCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/PhotoGallery/PhotoGallery.Application/Features/Images/Commands/DeleteImage/DeleteImageCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using PhotoGallery.Domain.Entities;
+using PhotoGallery.Domain.Interfaces.Repositories;
+using PhotoGallery.Domain.Interfaces.Services;
+
+namespace PhotoGallery.Application.Features.Images.Commands.DeleteImage
+{
+    public class DeleteImageCommandHandler : IRequestHandler<DeleteImageCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IImageService _imageService;
+
+        public DeleteImageCommandHandler(IUnitOfWork unitOfWork, IImageService imageService)
+        {
+            _unitOfWork = unitOfWork;
+            _imageService = imageService;
+        }
+
+        public async Task Handle(DeleteImageCommand request, CancellationToken cancellationToken)
+        {
+            var repo = _unitOfWork.GetRepository<Image>();
+
+            var imageToDelete = await repo.GetByIdAsync(request.Id);
+
+            if (imageToDelete == null)
+            {
+                throw new ArgumentException(
+                    $"Image with id: {request.Id} can't be found.", nameof(request.Id));
+            }
+
+            _imageService.DeleteImage(imageToDelete.FileName);
+
+            await _unitOfWork.SaveChangesAsync();
+        }
+    }
+}
