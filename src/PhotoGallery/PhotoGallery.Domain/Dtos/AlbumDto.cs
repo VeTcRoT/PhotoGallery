15ba2adc7ca10e49381c31d2c33800329bd7ea93
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-07 13:10:13 +0300

    Add AlbumDto to Domain

---
+++ src/PhotoGallery/PhotoGallery.Domain/Dtos/AlbumDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PhotoGallery.Domain.Dtos
+{
+    public class AlbumDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
