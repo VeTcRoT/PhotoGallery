3bf5cb8c5502504f5b5a07c075f9854126a3cc01
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-08 15:27:43 +0300

    Add Create Album, Delete Album and Shwo User Albums actions to controller

--- src/PhotoGallery/PhotoGallery.MVC/Controllers/HomeController.cs
+++ src/PhotoGallery/PhotoGallery.MVC/Controllers/HomeController.cs
@@ -1,9 +1,15 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PhotoGallery.Application.Features.Albums.Commands.CreateAlbum;
+using PhotoGallery.Application.Features.Albums.Commands.DeleteAlbum;
+using PhotoGallery.Application.Features.Albums.Queries.GetAlbumsByUserId;
 using PhotoGallery.Application.Features.Albums.Queries.ListPagedAlbums;
+using System.Security.Claims;
 
 namespace PhotoGallery.MVC.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly IMediator _mediator;
@@ -13,11 +19,34 @@ namespace PhotoGallery.MVC.Controllers
             _mediator = mediator;
         }
 
+        [AllowAnonymous]
         public async Task<IActionResult> Index(ListPagedAlbumsQuery query)
         {
             var albums = await _mediator.Send(query);
 
             return View(albums);
+        }
+
+        public async Task<IActionResult> UserAlbums(GetAlbumsByUserIdQuery query)
+        {
+            query.UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var albums = await _mediator.Send(query);
+            return View(albums);
+        }
+
+        public async Task<IActionResult> CreateAlbum(CreateAlbumCommand command)
+        {
+            await _mediator.Send(command);
+
+            return RedirectToAction(nameof(UserAlbums));
+        }
+
+        public async Task<IActionResult> DeleteAlbum(DeleteAlbumCommand command)
+        {
+            await _mediator.Send(command);
+
+            return RedirectToAction(nameof(UserAlbums));
         }
     }
 }
