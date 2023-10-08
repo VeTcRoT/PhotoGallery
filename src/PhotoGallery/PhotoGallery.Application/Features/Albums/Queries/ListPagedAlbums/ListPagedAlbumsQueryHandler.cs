b90fc2faeebabb0c00e57a886f16a5f3479ea334
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-08 10:08:48 +0300

    Modified Album query and query handler

--- src/PhotoGallery/PhotoGallery.Application/Features/Albums/Queries/ListPagedAlbums/ListPagedAlbumsQueryHandler.cs
+++ src/PhotoGallery/PhotoGallery.Application/Features/Albums/Queries/ListPagedAlbums/ListPagedAlbumsQueryHandler.cs
@@ -1,11 +1,11 @@
 ﻿using AutoMapper;
 using MediatR;
-using PhotoGallery.Domain.Dtos;
+using PhotoGallery.Domain.Helpers;
 using PhotoGallery.Domain.Interfaces.Repositories;
 
 namespace PhotoGallery.Application.Features.Albums.Queries.ListPagedAlbums
 {
-    public class ListPagedAlbumsQueryHandler : IRequestHandler<ListPagedAlbumsQuery, IReadOnlyCollection<ListPagedAlbumsDto>>
+    public class ListPagedAlbumsQueryHandler : IRequestHandler<ListPagedAlbumsQuery, PagedList<ListPagedAlbumsDto>>
     {
         private readonly IMapper _mapper;
         private readonly IUnitOfWork _unitOfWork;
@@ -16,12 +16,12 @@ namespace PhotoGallery.Application.Features.Albums.Queries.ListPagedAlbums
             _unitOfWork = unitOfWork;
         }
 
-        public async Task<IReadOnlyCollection<ListPagedAlbumsDto>> Handle(ListPagedAlbumsQuery request, CancellationToken cancellationToken)
+        public async Task<PagedList<ListPagedAlbumsDto>> Handle(ListPagedAlbumsQuery request, CancellationToken cancellationToken)
         {
             var pagedAlbums = await _unitOfWork.AlbumRepository
                 .GetPagedAlbumsAsync(request.PageNumber, request.PageSize);
 
-            return _mapper.Map<IReadOnlyCollection<ListPagedAlbumsDto>>(pagedAlbums);
+            return _mapper.Map<PagedList<ListPagedAlbumsDto>>(pagedAlbums);
         }
     }
 }
