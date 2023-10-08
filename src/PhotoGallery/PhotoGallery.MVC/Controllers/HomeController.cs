4fc0b274befd7e0060a0a013e50cea9c00065053
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-08 13:31:42 +0300

    Add Home controller

---
+++ src/PhotoGallery/PhotoGallery.MVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PhotoGallery.Application.Features.Albums.Queries.ListPagedAlbums;
+
+namespace PhotoGallery.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public HomeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Index(ListPagedAlbumsQuery query)
+        {
+            if (query.PageNumber == 0 && query.PageSize == 0)
+            {
+                query.PageNumber = 1;
+                query.PageSize = 5;
+            }
+
+            if (query.PageSize > 10)
+                query.PageSize = 10;
+
+            var albums = await _mediator.Send(query);
+
+            return View(albums);
+        }
+    }
+}
