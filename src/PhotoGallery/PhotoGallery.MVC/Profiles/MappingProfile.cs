9e4b0382beef3f1858b3dce85127ab4608b34278
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-08 22:21:35 +0300

    Add mapping between DeleteImageDto and DeleteImageCommand

---
+++ src/PhotoGallery/PhotoGallery.MVC/Profiles/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PhotoGallery.Application.Features.Images.Commands.DeleteImage;
+using PhotoGallery.MVC.Models;
+
+namespace PhotoGallery.MVC.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<DeleteImageDto, DeleteImageCommand>();
+        }
+    }
+}
