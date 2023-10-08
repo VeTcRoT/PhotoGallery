2b9479e9e4b125b839d63c765358bea8d9a695c7
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-08 10:46:09 +0300

    Add Statup Helper

---
+++ src/PhotoGallery/PhotoGallery.MVC/StartupHelperExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PhotoGallery.Application;
+using PhotoGallery.Domain.Entities;
+using PhotoGallery.Persistence.Data;
+using PhotoGallery.Persistence;
+
+namespace PhotoGallery.MVC
+{
+    internal static class StartupHelperExtensions
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddApplicationServices();
+            builder.Services.AddPersistenceServices();
+
+            builder.Services.AddDbContext<PhotoGalleryDbContext>(options =>
+            {
+                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+            });
+
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<PhotoGalleryDbContext>();
+
+            return builder.Build();
+        }
+
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            app.UseStaticFiles();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapDefaultControllerRoute();
+            });
+
+            return app;
+        }
+    }
+}
