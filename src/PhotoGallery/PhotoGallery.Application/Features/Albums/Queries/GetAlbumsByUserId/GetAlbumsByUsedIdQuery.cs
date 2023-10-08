2760953f9a29c605871f693ab8286b011480e695
Vitalij Hnativ <vitalijhnativ@gmail.com>
2023-10-08 09:30:04 +0300

    Add dto and query

---
+++ src/PhotoGallery/PhotoGallery.Application/Features/Albums/Queries/GetAlbumsByUserId/GetAlbumsByUsedIdQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace PhotoGallery.Application.Features.Albums.Queries.GetAlbumsByUserId
+{
+    public class GetAlbumsByUsedIdQuery
+    {
+        public string UserId { get; set; } = string.Empty;
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+    }
+}
